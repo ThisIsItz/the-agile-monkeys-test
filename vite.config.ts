733d9cdd9669633b69aa1998d093e2e8ca2f4e33
Itziar Urbieta <itziarurbieta94@gmail.com>
2026-08-12 08:42:02 +0100

    chore: configure path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from 'node:path'
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
@@ -6,6 +7,13 @@ export default defineConfig({
   test: {
     env: {
       DB_PATH: ':memory:'
+    }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(import.meta.dirname, 'src'),
+      '@shared': path.resolve(import.meta.dirname, 'shared'),
+      '@server': path.resolve(import.meta.dirname, 'server')
     }
   }
 })
